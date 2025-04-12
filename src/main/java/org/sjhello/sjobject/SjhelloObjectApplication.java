6e4221e42dd48b8cd9536b73b04caf41517f1d90
sjhello <sjhello235@gmail.com>
2025-04-12 23:52:37 +0900

    init

---
+++ src/main/java/org/sjhello/sjobject/SjhelloObjectApplication.java
@@ -0,0 +1,13 @@
+package org.sjhello.sjobject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SjhelloObjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SjhelloObjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sjhello/sjobject/SjhelloObjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sjhello.sjobject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SjhelloObjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
