f1702c4b094b6daced4492f7efb99582ca43f61a
sjhello <sjhello235@gmail.com>
2025-04-20 12:27:25 +0900

    chapter5: 5장 예제

---
+++ src/main/java/org/sjhello/sjobject/chapter5/DiscountConditionType.java
@@ -0,0 +1,6 @@
+package org.sjhello.sjobject.chapter5;
+
+public enum DiscountConditionType {
+	PERIOD,
+	SEQUENCE
+}
---
+++ src/main/java/org/sjhello/sjobject/chapter5/Movie.java
@@ -0,0 +1,57 @@
+package org.sjhello.sjobject.chapter5;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.sjhello.sjobject.chapter5.money.Money;
+
+public class Movie {
+	private String title;
+	private Duration runningTime;
+	private Money fee;
+	private List<DiscountCondition> discountConditions;
+
+	private MovieType movieType;
+	private Money discountAmount;
+	private double discountPercent;
+
+	public Money calculateMovieFee(Screening screening) {
+		if (isDiscountable(screening)) {
+			return fee.minus(calculateDiscountAmount());
+		}
+		return fee;
+	}
+
+	private Money calculateDiscountAmount() {
+		switch (movieType) {
+			case AMOUNT_DISCOUNT -> {
+				return calculateAmountDiscountAmount();
+			}
+			case PERCENT_DISCOUNT -> {
+				return calculatePercentDiscountAmount();
+			}
+			case NONE_DISCOUNT -> {
+				return calculateNoneDiscountAmount();
+			}
+		}
+
+		return null;
+	}
+
+	private Money calculateNoneDiscountAmount() {
+		return Money.ZERO;
+	}
+
+	private Money calculatePercentDiscountAmount() {
+		return fee.times(discountPercent);
+	}
+
+	private Money calculateAmountDiscountAmount() {
+		return discountAmount;
+	}
+
+	private boolean isDiscountable(Screening screening) {
+		return discountConditions.stream()
+			.anyMatch(condition -> condition.isSatisfiedBy(screening));
+	}
+}
